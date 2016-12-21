e4741daf0ff1b13d8a3ed4018e7e407634bcc76a
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-12-21 17:50:29 +0800

    gulp 文件优化

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,47 @@
 var gulp = require('gulp');
 var debug = require('gulp-debug');
-var changed = require('gulp-changed');
+var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
-var minifyCss = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+var cssmin = require('gulp-minify-css');
+var jsmin = require('gulp-uglify');
+var imgmin = require('gulp-imagemin');
+var del = require('del');
+var vinylpaths = require('vinyl-paths');
+
+var minify = function (root, extname, action) {
+    var include = root + '/**/*' + extname;
+    var exclude = '!' + root + '/**/*.min' + extname;
+    gulp.src([include, exclude])
+        .pipe(vinylpaths(del))
+        .pipe(action)
+        .pipe(rename(function (path) {
+            var extname = path.extname;
+            path.extname = '.min' + extname;
+        }))
+        .pipe(gulp.dest(root))
+        .pipe(debug());
+};
 
 gulp.task('minify_demos', function () {
-    var srcPath = './demosSrc';
-    var targetPath = './demos';
-    var htmlPath = srcPath + '/**/*.html';
-    var jsPath = srcPath + '/**/*.js';
-    var cssPath = srcPath + '/**/*.css';
-    var imgPath = srcPath + '/**/*.{png,jpg}';
-    var minJsPath = srcPath + '/**/*.min.js';
-    var minCssPath = srcPath + '/**/*.min.css';
-
-    gulp.src(htmlPath)
-        .pipe(htmlmin({
+    var root = './demos';
+    minify(root, '.html', htmlmin({
             removeComments: true,
             collapseWhitespace: true,
             minifyJS: true,
             minifyCSS: true
-        }))
-        .pipe(changed(targetPath))
-        .pipe(gulp.dest(targetPath))
-        .pipe(debug({title: 'html:'}));
-
-    gulp.src([jsPath, '!' + minJsPath])
-        .pipe(uglify())
-        .pipe(changed(targetPath))
-        .pipe(gulp.dest(targetPath))
-        .pipe(debug({title: 'js:'}));
-    gulp.src(minJsPath)
-        .pipe(changed(targetPath))
-        .pipe(gulp.dest(targetPath))
-        .pipe(debug({title: 'min.js:'}));
-
-    gulp.src([cssPath, '!' + minCssPath])
-        .pipe(minifyCss())
-        .pipe(changed(targetPath))
-        .pipe(gulp.dest(targetPath))
-        .pipe(debug({title: 'css:'}));
-    gulp.src(minCssPath)
-        .pipe(changed(targetPath))
-        .pipe(gulp.dest(targetPath))
-        .pipe(debug({title: 'min.css:'}))
-
-    gulp.src(imgPath)
-        .pipe(imagemin({
-            progressive: true,
-            use: [pngquant()]
-        }))
-        .pipe(changed(targetPath))
-        .pipe(gulp.dest(targetPath))
-        .pipe(debug({title: 'img:'}));
+        })
+    );
+    minify(root, '.css', cssmin());
+    minify(root, '.js', jsmin());
+    minify(root, '.{png,jpg}', imgmin({
+            progressive: true
+        })
+    );
 });
 gulp.task('minify_materials', function () {
-    var path = './materials';
-    var pattern = path + '/**/*.{png,jpg}';
-    gulp.src(pattern)
-        .pipe(imagemin({
-            progressive: true,
-            use: [pngquant()]
-        }))
-        .pipe(changed(path))
-        .pipe(gulp.dest(path))
-        .pipe(debug({title: 'img:'}));
+    var root = './materials';
+    minify(root, '.{png,jpg}', imgmin({
+            progressive: true
+        })
+    );
 });

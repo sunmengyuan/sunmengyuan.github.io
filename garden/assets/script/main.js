eb9f066ed5858741e3ac10f5d38f926e64efe071
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-12-27 11:06:02 +0800

    menu按钮旋转动画

--- garden/assets/script/main.js
+++ garden/assets/script/main.js
@@ -1,1 +1,22 @@
-console.log('执行');
+var main = {
+    do: function () {
+        var _this = this;
+        _this.menuHandle();
+    },
+    menuHandle: function () {
+        var $menu = $('.js_menu');
+        var $btn = $('.js_menu_btn');
+        $btn.on('click', function () {
+            var status = $(this).hasClass('close');
+            if (status) {
+                $(this).removeClass('rollingF close').addClass('rollingB');
+                $menu.slideUp();
+            } else {
+                $(this).removeClass('rollingB').addClass('rollingF close');
+                $menu.slideDown();
+            }
+        });
+    }
+};
+
+main.do();

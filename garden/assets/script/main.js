74e4aaae668a361db6eca47a1a563b5ebf6b2845
sunmy <sunmengyuan@wanmeizhensuo.com>
2017-01-09 18:53:11 +0800

    分页逻辑

--- garden/assets/script/main.js
+++ garden/assets/script/main.js
@@ -2,6 +2,7 @@ var main = {
     do: function () {
         var _this = this;
         _this.menuHandle();
+        _this.paginator();
 
         hljs.initHighlightingOnLoad();
     },
@@ -18,6 +19,50 @@ var main = {
                 $menu.slideDown();
             }
         });
+    },
+    paginator: function () {
+        var $paginator = $('.js_paginator');
+        var offset = 2;
+        var count = 2 * offset + 1;
+        var cur = pageData.curPage >> 0;
+        var total = pageData.totalPage >> 0;
+
+        if (cur < count - offset) {
+            for (var i = 1;i <= (total < count ? total : count);i++) {
+                var $item = $(document.createElement('a'))
+                    .text(i)
+                    .attr('href', pageData.postsUrl + '/' + (i == 1 ? '' : i));
+                $paginator.append($item);
+            }
+            return;
+        }
+
+        if (cur > total - offset) {
+            for (var j = total;j > (total < count ? 0 : total - count);j--) {
+                var $item = $(document.createElement('a'))
+                    .text(j)
+                    .attr('href', pageData.postsUrl + '/' + (j == 1 ? '' : j));
+                $paginator.prepend($item);
+            }
+            return;
+        }
+
+        var $cur = $(document.createElement('a'))
+            .text(cur)
+            .attr('href', pageData.postsUrl + '/' + (cur == 1 ? '' : cur));
+        $paginator.append($cur);
+        for (var m = 1;m <= offset;m++) {
+            var pre = cur - m;
+            var next = cur + m;
+            var $pre = $(document.createElement('a'))
+                .text(pre)
+                .attr('href', pageData.postsUrl + '/' + (pre == 1 ? '' : pre));
+            var $next = $(document.createElement('a'))
+                .text(next)
+                .attr('href', pageData.postsUrl + '/' + (next == 1 ? '' : next));
+            $paginator.prepend($pre);
+            $paginator.append($next);
+        }
     }
 };
 
